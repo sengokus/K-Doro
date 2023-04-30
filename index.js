3927363b2de2a87324b063fc0285dd87ee185417
nico <90756549+sengokus@users.noreply.github.com>
2023-04-30 22:05:41 +0800

    Updated login, registration, and unit/group select with working JS

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ import { firebaseConfig } from "./firebaseConfig.js"
 
 firebaseConfig();
 addUser();
+
+// change group.html to group user selected
+function setGroup(group) {
+    var groupContainers = document.querySelectorAll(".group-select");
+  
+    for (var i = 0; i < groupContainers.length; i++) {
+      groupContainers[i].style.display = "none";
+    }
+  
+    var selectedGroup = document.getElementById("group-" + group);
+    selectedGroup.style.display = "flex";
+    console.log(selectedGroup);
+  }
