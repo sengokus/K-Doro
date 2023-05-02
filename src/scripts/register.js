53536a6a55d7c77746c87961489805b2aff65235
nico <90756549+sengokus@users.noreply.github.com>
2023-05-02 20:30:40 +0800

    Fixed register.js pathing

--- src/scripts/register.js
+++ src/scripts/register.js
@@ -1,5 +1,5 @@
 import { addDoc } from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js';
-import { db } from "../index.js";
+import { db } from "../../index.js";
 import { collection } from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js';
 
 
@@ -13,33 +13,38 @@ registerForm.addEventListener("submit", (event) => {
   // Get the user's email, username, and password
   const email = document.getElementById("user_email").value;
   const username = document.getElementById("user_name").value;
-  const password = document.getElementById("user_password").value;
+  const password1 = document.getElementById("user_password1").value;
+  const password2 = document.getElementById("user_password2").value;
 
   // TODO: Use the email, username, and password to register the user
-  console.log(`Registering user: ${email}, ${username}, ${password}`);
-  registerUser(email,username,password);
+  console.log(`Registering user: ${email}, ${username}, ${password1}`);
+  registerUser(email,username,password1, password2);
 });
 
 
-export async function registerUser(email, username, password){
+export async function registerUser(email, username, password1, password2){
 
     try {
-        if (username === "" || username === " " || password === " " || password === "") {
+        if (username === "" || username === " " || password1 === " " || password1 === "") {
             throw new Error('Username or Password is empty');
             }
-        
-            if (password.length < 3) {
-            throw new Error('Password is weak');
+            
+            if (password1 != password2) {
+              throw new Error('Please make sure passwords match.');
             }
-        
-        
-            try {
+            if (password1.length < 3) {
+              throw new Error('Password is weak');
+            }
+             try {
                 const docRef = await addDoc(collection(db, "users"), {
                 email: email,
                 username: username,
-                password: password
+                password: password2,
                 });
                 console.log("Document written with ID: ", docRef.id);
+                alert("Registered successfully");
+                document.getElementById("register").reset();
+                
             } catch (e) {
                 console.error("Error adding document: ", e);
             }
